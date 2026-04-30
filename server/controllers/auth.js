0cac9b342c8625a4c412c98e2231c939674c3146
Chythanya-v <chythanya.v27@gmail.com>
2026-05-01 05:02:24 +0530

    added login API and JWT authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import { hashSync, compareSync } from "bcrypt";
 import { prisma } from "../prisma.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup = async (req, res) => {
@@ -32,6 +33,29 @@ export const signup = async (req, res) => {
         return res.status(201).json({ message: "User created successfully", user });
     } catch (error) {
         console.error("Signup error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        if (!compareSync(password, user.password)) {
+            return res.status(400).json({ message: "Incorrect Password" });
+        }
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+        return res.status(200).json({ message: "User logged in successfully", email: user.email, token });
+    } catch (error) {
+        console.error("Login error:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
