013dd700f27f27d62142c5e21b713c65cd0c18d6
Chythanya-v <chythanya.v27@gmail.com>
2026-05-02 10:59:49 +0530

    added authentication middleware

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,25 +1,15 @@
 import { Router } from "express";
+import { authenticate } from "../middleware/auth.js";
+import { getProducts, createProduct, getProductById, updateProduct, deleteProduct } from "../controllers/products.js";
 
 const productsRouter = Router();
 
-productsRouter.get("/", (req, res) => {
-    res.json({ message: "Get all products" });
-});
+productsRouter.use(authenticate); // All product routes require authentication
 
-productsRouter.post("/", (req, res) => {
-    res.json({ message: "Create a new product" });
-});
-
-productsRouter.get("/:id", (req, res) => {
-    res.json({ message: `Get product with ID ${req.params.id}` });
-});
-
-productsRouter.put("/:id", (req, res) => {
-    res.json({ message: `Update product with ID ${req.params.id}` });
-});
-
-productsRouter.delete("/:id", (req, res) => {
-    res.json({ message: `Delete product with ID ${req.params.id}` });
-});
+productsRouter.get("/", getProducts);
+productsRouter.post("/", createProduct);
+productsRouter.get("/:id", getProductById);
+productsRouter.put("/:id", updateProduct);
+productsRouter.delete("/:id", deleteProduct);
 
 export default productsRouter;
